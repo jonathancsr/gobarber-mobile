8abe1c6cdbf56b72caa733fd04788c195ccc133b
jonathancsr <jonathancsrr@gmail.com>
2020-07-07 20:23:24 -0300

    first commit

---
+++ src/pages/SignOut/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const SignOut: React.FC = () => {
+  return (
+    <>
+      <Container />
+    </>
+  );
+};
+
+export default SignOut;
