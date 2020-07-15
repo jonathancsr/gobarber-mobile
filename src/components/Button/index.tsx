88bdce8639fdec1af21fa52f671bbe2b512482c4
jonathancsr <jonathancsrr@gmail.com>
2020-07-15 19:46:44 -0300

    add: Button logic

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { RectButtonProperties } from 'react-native-gesture-handler';
 
-import { Container } from './styles';
+import { Container, ButtonText } from './styles';
 
-const Button: React.FC = () => <Container />;
+interface ButtonProps extends RectButtonProperties {
+  children: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
+  return (
+    <Container {...rest}>
+      <ButtonText>{children}</ButtonText>
+    </Container>
+  );
+};
 
 export default Button;
